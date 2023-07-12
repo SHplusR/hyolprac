b575798853a856e36fa5000ef6f1b6da8ba5d14b
SHplusR <111962797+SHplusR@users.noreply.github.com>
2023-07-12 18:04:09 +0900

    thymleaf 수정

--- src/main/java/com/example/hyolprac/controller/HomeController.java
+++ src/main/java/com/example/hyolprac/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.example.hyolprac.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,12 +15,27 @@ public class HomeController {
     }
     @GetMapping("/login")
     public void login(){
-
+        System.out.println("login page");
     }
-
     @GetMapping("/signup")
     public void signup(){
-
+        System.out.println("signup page");
     }
 
+@GetMapping("/board")
+public void board(){
+    System.out.println("get board");
+}
+    @PostMapping("/board")
+    public void postBoard(){
+
+    }
+    @GetMapping("/register")
+    public void register(){
+        System.out.println("register");
+    }
+    @PostMapping("/register")
+    public void postRegister(){
+
+    }
 }
